a9d5dea93f3ab03060ce66a6e9ca71645d329454
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2023-12-01 11:33:18 +0700

    fix cors again for ports

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:*",
 ]
 
 app.add_middleware(
