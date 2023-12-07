b1d5e963ddfaaca283b0117f1adca3c8bdb20046
Tran Cong Toan <22110248@student.hcmute.edu.vn>
2023-12-07 10:21:58 +0700

    Frontend: Upload frontend

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
+    "https://www.thunderclient.com",
     "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:*",
